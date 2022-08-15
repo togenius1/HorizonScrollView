54983d31627a601febe679abee0a60462d8be03f
Todsaporn Laosa <togenius1@gmail.com>
2022-08-15 22:22:37 +0700

    completed

--- App.tsx
+++ App.tsx
@@ -1,16 +1,66 @@
 import React from 'react';
-import {SafeAreaView, Text, useColorScheme, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import Carousel from './components/Carousel';
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
+export default function App() {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>App</Text>
-      </View>
-    </SafeAreaView>
-  );
-};
+    <View style={styles.container}>
+      <Text>Statistics Carousel:</Text>
+      <Carousel
+        style="stats"
+        itemsPerInterval={3}
+        items={[
+          {
+            label: 'TODAY',
+            value: 1,
+          },
+          {
+            label: 'THIS WEEK',
+            value: 39,
+          },
+          {
+            label: 'THIS MONTH',
+            value: 120,
+          },
+          {
+            label: 'YESTERDAY',
+            value: 3,
+          },
+          {
+            label: 'LAST WEEK',
+            value: 25,
+          },
+          {
+            label: 'LAST MONTH',
+            value: 175,
+          },
+        ]}
+      />
 
-export default App;
+      <Text style={{marginTop: 30}}>Intro Slides Carousel:</Text>
+      <Carousel
+        style="slide"
+        items={[
+          {
+            title: 'Welcome, swipe to continue.',
+          },
+          {
+            title: 'About feature X.',
+          },
+          {
+            title: 'About feature Y.',
+          },
+        ]}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+});
