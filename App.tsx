6ccc2ffb1c9c3fea1c698d8ebe926d60cf19c679
togenius1 <90688382+togenius1@users.noreply.github.com>
2022-08-16 14:09:41 +0700

    new coding style

--- App.tsx
+++ App.tsx
@@ -1,66 +1,121 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import Carousel from './components/Carousel';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  Dimensions,
+  Animated,
+} from 'react-native';
+import React, {useRef} from 'react';
 
-export default function App() {
+type Props = {};
+
+const images = [
+  {id: 1, img: require('./assets/home1.jpg'), title: 'Home1', color: '#33ccff'},
+  {id: 2, img: require('./assets/home2.jpg'), title: 'Home2', color: '#ff33be'},
+  {id: 3, img: require('./assets/home3.jpg'), title: 'Home3', color: '#33ccff'},
+  {id: 4, img: require('./assets/home4.jpg'), title: 'Home4', color: '#ff33d6'},
+  {id: 5, img: require('./assets/home5.jpg'), title: 'Home5', color: '#33ccff'},
+  {id: 6, img: require('./assets/home6.jpg'), title: 'Home6', color: '#ff339c'},
+];
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const App = (props: Props) => {
+  const scrollX = useRef(new Animated.Value(0)).current;
+
   return (
-    <View style={styles.container}>
-      <Text>Statistics Carousel:</Text>
-      <Carousel
-        style="stats"
-        itemsPerInterval={3}
-        items={[
-          {
-            label: 'TODAY',
-            value: 1,
-          },
-          {
-            label: 'THIS WEEK',
-            value: 39,
-          },
-          {
-            label: 'THIS MONTH',
-            value: 120,
-          },
-          {
-            label: 'YESTERDAY',
-            value: 3,
-          },
-          {
-            label: 'LAST WEEK',
-            value: 25,
-          },
-          {
-            label: 'LAST MONTH',
-            value: 175,
-          },
-        ]}
-      />
+    <View>
+      <View
+        style={{
+          height: 50,
+          backgroundColor: 'red',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Text>Header</Text>
+      </View>
+      <ScrollView scrollEventThrottle={16}>
+        <View style={{flex: 1, backgroundColor: 'white', paddingTop: 20}}>
+          <Text
+            style={{fontSize: 24, fontWeight: '700', paddingHorizontal: 20}}>
+            What can we help you find, Home
+          </Text>
 
-      <Text style={{marginTop: 30}}>Intro Slides Carousel:</Text>
-      <Carousel
-        style="slide"
-        items={[
-          {
-            title: 'Welcome, swipe to continue.',
-          },
-          {
-            title: 'About feature X.',
-          },
-          {
-            title: 'About feature Y.',
-          },
-        ]}
-      />
+          <View style={{height: 200, marginTop: 20}}>
+            <ScrollView
+              horizontal={true}
+              pagingEnabled={true}
+              showsHorizontalScrollIndicator={false}
+              onScroll={Animated.event(
+                [{nativeEvent: {contentOffset: {x: scrollX}}}],
+                {useNativeDriver: false},
+              )}>
+              {images.map((image, imageIndex) => {
+                return (
+                  <Animated.View>
+                    <Image source={image.img} style={[styles.card]} />
+                  </Animated.View>
+                );
+              })}
+            </ScrollView>
+          </View>
+
+          {/* Dots */}
+          <View style={styles.indicatorContainer}>
+            {images.map((image, imageIndex) => {
+              const width: number = scrollX.interpolate({
+                inputRange: [
+                  windowWidth * (imageIndex - 1),
+                  windowWidth * imageIndex,
+                  windowWidth * (imageIndex + 1),
+                ],
+                outputRange: [8, 16, 8],
+                extrapolate: 'clamp',
+              });
+              console.log(width);
+              return (
+                <Animated.View
+                  style={[
+                    styles.normalDots,
+                    {
+                      width,
+                      // height: width,
+                      backgroundColor: image.color,
+                    },
+                  ]}
+                />
+              );
+            })}
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
+  card: {
     flex: 1,
-    alignItems: 'center',
+    marginVertical: 10,
+    width: 350,
+    overflow: 'hidden',
+    alignSelf: 'center',
+  },
+  indicatorContainer: {
+    flexDirection: 'row',
     justifyContent: 'center',
-    padding: 10,
+    alignItems: 'center',
+  },
+  normalDots: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginHorizontal: 4,
+    marginTop: 8,
   },
 });
